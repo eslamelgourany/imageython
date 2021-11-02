3bc8ad507bdeb0d28dd65e7b6ea773859f28cbfa
Eslam Elgourany <eslam.elgourany@avast.com>
2021-11-02 19:53:55 +0100

    Adding code for review by Udacity

---
+++ imageython/test_package.py
@@ -0,0 +1,5 @@
+from imageython import Processimages
+from imageython import Loadimages
+
+
+Loadimages.read_image(file_path="images/")
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='imageython',
+      version='0.1',
+      description='imageython',
+      packages=['imageython'],
+      zip_safe=False)
